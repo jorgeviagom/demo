7d753c590d7119fad9bdefd6f36d73513e261f7c
jorgeviagom <jvg1020@hotmail.com>
2025-10-01 20:32:09 +0200

    Initial commit

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("!Hello World!");
+        File fichero = new File("d:\\Prueba\\miFichero.docx");
+        try{
+            if(fichero.createNewFile()){
+                System.out.println("El fichero se ha creado correctamente.");
+            }else{
+                System.out.println("El fichero ya existe");
+            }
+        } catch(IOException ioe){
+            System.out.println("Se ha producido un error ");
+        }
     }
 }
